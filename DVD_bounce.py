2a39770493d8a950043cd35db6da7f8af0b1073c
Prats Mathur <prats.mathur@gmail.com>
2021-08-15 22:10:17 -0700

    working intersection calculation

--- DVD_bounce.py
+++ DVD_bounce.py
@@ -1,24 +1,44 @@
 import numpy as np
+import math
 
 class BounceBox:
-    def __init__(self, xcor, ycor, h, w, vectorVelocity):
+    def __init__(self, xcor, ycor, h, w, direction, vector_mag=5):
         ''' Instantiator function. Inputs
-                scalars: starting xcor, ycor, static height, static width
-                list   : vector velocity in form [xvel, yvel]
+                scalars: starting xcor, ycor, static height, static width, velocity as an angle in radians
         '''
         self.xcor = xcor
         self.ycor = ycor
         self._h = h / 2
         self._w = w / 2
-        self.velocity = vectorVelocity
+        self.velocity = np.array([math.cos(direction), math.sin(direction)])
             # To do: replace with numpy array
     
     def is_collision(self, boundingBoxObject):
         H, W = boundingBoxObject.getedges()
 
-        if (self.xcor - self._h < 0 ) or (self.ycor - self._w < 0) or (self.xcor + self._h > H) or (self.ycor + self._w > W):
+        if (self.xcor - self._w <= 0 ) or (self.ycor - self._h < 0) or (self.xcor + self._w > H) or (self.ycor + self._h > W):
             return True
         return False
+
+    def calculate_next_intersectionpoint(self, boundingBoxObject):
+        boundingdim = boundingBoxObject.getedges()
+
+        edges = [0, 0, boundingdim[0], boundingdim[1]]
+        
+        t = None
+
+        for idx in range(0,4):
+            # convention, evens are intersections with left/right, odds are up down
+            if idx % 2 == 0:
+                t = (edges[idx] - self.xcor) / self.velocity[0]
+                if t < boundingdim[0] and t > 0:
+                    break
+            else:
+                t = (edges[idx] - self.ycor) / self.velocity[1]
+                if t < boundingdim[1] and t > 0:
+                    break
+
+        return (self.velocity[0]*t + self.xcor, self.velocity[1]*t + self.ycor)
     
     def __str__(self):
         return f'Bounce Box Stats: \n\tPosition: [{self.xcor} , {self.ycor}] \n\tVelocity: [{self.velocity}] \n\tDimensions: [{self._h} , {self._w}]'
@@ -38,10 +58,11 @@ class BoundingBox:
         return [self._H, self._W]
 
 def main():
-    Bouncer = BounceBox(15 , 15, 1, 1, [1, 0])
-    Box = BoundingBox([30,30])
+    angle = 3
+    Bouncer = BounceBox(6, 4, 1, 1, angle)
+    Box = BoundingBox([10,10])
 
-    
+    print(Bouncer.calculate_next_intersectionpoint(Box))    
 
 
 if __name__ == '__main__':
